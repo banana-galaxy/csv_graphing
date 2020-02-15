a5c362cbca1d80943411e6d78c12e4a7d1ff2efc
banana-galaxy <cawasp@gmail.com>
2020-02-14 18:34:36 -0800

    initial commit

--- graph.py
+++ graph.py
@@ -1,14 +1,17 @@
-from matplotlib import pyplot
+'''from matplotlib import pyplot
 import sys
 
 if len(sys.argv) == 1:
     print("Please enter a file to plot as an argument")
     quit()
 
+color = []
 title = input("Enter the graph title (leave blank for no title): ")
 xmin = input("Enter x axis minimum (leave blank for no limits): ")
 if xmin != "":
     xmax = input("Enter x axis maximum: ")
+for csv in range(len(sys.argv)-1):
+    color.append(input(f"Enter color for {sys.argv[csv+1]} (leave blank for default sky blue): "))
 head_x = ""
 head_y = ""
 x = []
@@ -24,21 +27,68 @@ with open(sys.argv[1], "r") as f:
                 x.append(float(line[0]))
                 y.append(float(line[1]))
 
-fig = pyplot.figure(figsize=(8,6), dpi=100)
-fig.suptitle(title)
-plot = fig.add_subplot()
-plot.plot(x,y)
-plot.set_xlabel(head_x)
-plot.set_ylabel(head_y)
-#plot.title(title)
-plot.grid()
-pyplot.xscale('log')
-'''pyplot.plot(x,y)
+
+for csv in range(len(sys.argv)-1):
+    if color[csv] != '':
+        pyplot.plot(x[csv], y[csv], color=color[csv])
+    else:
+        pyplot.plot(x[csv], y[csv])
+
+if xmin != '':
+    pyplot.set_xlim([int(xmin), int(xmax)])
 pyplot.xlabel(head_x)
 pyplot.ylabel(head_y)
 pyplot.title(title)
-pyplot.grid()
-pyplot.xscale('log')'''
+pyplot.grid(True)
+pyplot.xscale('log')
+pyplot.show()'''
+
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) == 1:
+    print("Please enter a file to plot as an argument")
+    quit()
+
+color = []
+title = input("Enter the graph title (leave blank for no title): ")
+xmin = input("Enter x axis minimum (leave blank for no limits): ")
+if xmin != "":
+    xmax = input("Enter x axis maximum: ")
+for csv in range(len(sys.argv)-1):
+    color.append(input(f"Enter color for {sys.argv[csv+1]} (leave blank for default sky blue): "))
+head_x = ""
+head_y = ""
+x = []
+y = []
+for csv in range(len(sys.argv)-1):
+    with open(sys.argv[csv+1], "r") as f:
+        x.append([])
+        y.append([])
+        for line in f:
+            if line != ['']:
+                line = line.split(",")
+                if '"' in line[0]:
+                    head_x = line[0].replace('"', '')
+                    head_y = line[1].replace('"', '')
+                else:
+                    x[csv].append(float(line[0]))
+                    y[csv].append(float(line[1]))
+
+fig = plt.figure(figsize=(8,6))
+plot = fig.add_subplot()
+
+for count in range(len(sys.argv)-1):
+    if color[count] != '':
+        plot.plot(x[count], y[count], color[count])
+    else:
+        plot.plot(x[count], y[count])
+
+plot.set_xlabel(head_x)
+plot.set_ylabel(head_y)
+plt.title(title)
 if xmin != '':
-    plot.set_xlim([int(xmin), int(xmax)])
-pyplot.show()
+    plt.xlim([int(xmin), int(xmax)])
+plt.xscale('log')
+plt.grid(True)
+plt.show()
