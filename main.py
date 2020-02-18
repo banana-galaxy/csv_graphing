f9dd0aab1f93a46bce0100ac3012c50b726c977a
banana-galaxy <cawasp@gmail.com>
2020-02-17 20:50:56 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys, os
+from matplotlib import pyplot as pyplot
+from assemble import assemble
+from callibrate import callibrate
+from graph import graph
+
+files = []
+for f in os.listdir(os.getcwd()):
+    files.append(f)
+
+comm = []
+diff = []
+unsym = []
+cal = []
+floor = []
+assembled = []
+calibrated = []
+files = {"cal":cal, "floor":floor, "comm":comm, "diff":diff, "unsym":unsym, "assembled":assembled, "calibrated":calibrated}
+names = ["cal", "floor", "comm", "diff", "unsym"]
+show = ["floor", "comm", "diff", "unsym"]
+
+for f in os.listdir(os.getcwd()):
+    for csv in files:
+        if csv in f and f.endswith(".csv"):
+            files[csv].append(f)
+
+for f in names:
+    args = ["program"]
+    for csv in files[f]:
+        args.append(csv)
+    assemble(args)
+    os.rename("output.csv", f"{f}_assembled.csv")
+    files["assembled"].append(f"{f}_assembled.csv")
+
+for f in range(1, len(files["assembled"])):
+    callibrate(["program", files["assembled"][0], files["assembled"][f]])
+    parts = files["assembled"][f].split(".")
+    csv = parts[0] + "_calibrated." + parts[1]
+    files["calibrated"].append(csv)
+
+output = ["program"]
+for f in calibrated:
+    output.append(f)
+graph(output)
