26af6db9a0931e340a27bea8a9b62fff0daed427
banana-galaxy <cawasp@gmail.com>
2020-02-17 21:25:11 -0800

    auto deleting files

--- main.py
+++ main.py
@@ -22,17 +22,24 @@ show = ["floor", "comm", "diff", "unsym"]
 for f in os.listdir(os.getcwd()):
     for csv in files:
         if csv in f and f.endswith(".csv"):
-            files[csv].append(f)
+            if not "assemble" in f:
+                files[csv].append(f)
 
 for f in names:
     args = ["program"]
     for csv in files[f]:
         args.append(csv)
     assemble(args)
+    if os.path.exists(f"{f}_assembled.csv"):
+        os.remove(f"{f}_assembled.csv")
     os.rename("output.csv", f"{f}_assembled.csv")
     files["assembled"].append(f"{f}_assembled.csv")
 
 for f in range(1, len(files["assembled"])):
+    parts = files["assembled"][f].split(".")
+    csv = parts[0] + "_calibrated." + parts[1]
+    if os.path.exists(csv):
+        os.remove(csv)
     callibrate(["program", files["assembled"][0], files["assembled"][f]])
     parts = files["assembled"][f].split(".")
     csv = parts[0] + "_calibrated." + parts[1]
