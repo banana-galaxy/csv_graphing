dce7d364aa4766312a24cc70a744fc57e481810a
banana-galaxy <cawasp@gmail.com>
2020-02-17 20:50:26 -0800

    can now run alone or be called

--- assemble.py
+++ assemble.py
@@ -1,73 +1,79 @@
 import sys, os
 
-# accounting for human factor
-if len(sys.argv) == 1:
-    print("Please provide paths to the files as arguments")
-    quit()
 
-files = []
-for f in range(1, len(sys.argv)):
-    if not os.path.exists(sys.argv[f]):
-        if "*" in sys.argv[f]:
-            template = sys.argv[f].replace("*", "")
-            found = False
-            for csv in os.listdir(os.getcwd()):
-                if template in csv:
-                    files.append(csv)
-                    found = True
-            if not found:
-                print(f"Could not find files based on {template}\nplease make sure the script and the files are in the same directory")
-                quit()
-        else:
-            print(f"Could not find {sys.argv[f]}")
-            quit()
-    else:
-        files.append(sys.argv[f])
-
-data = []
-
-for f in files: # assembling all the data into one multi-dimensional list
-    with open(f, "r") as csv:
-        content = csv.read().split("\n")
-        for line in range(len(content)):
-            content[line] = content[line].split(",")
-            if not '"' in content[line][0]:
-                for instance in range(len(content[line])):
-                    if content[line] != ['']:
-                        content[line][instance] = float(content[line][instance])
-            
-        data.append(content)
-
-head = data[0][0]
-for f in data:
-    if not f[0] == head:
-        print("The files do not have the same headers")
+def assemble(args):
+    # accounting for human factor
+    if len(args) == 1:
+        print("Please provide paths to the files as arguments")
         quit()
 
-#deciding on the name of the output file
-output = ""
-if os.path.exists("output.csv"):
-    count = 1
-    while True:
-        if os.path.exists(f"output{count}.csv"):
-            count += 1
+    files = []
+    for f in range(1, len(args)):
+        if not os.path.exists(args[f]):
+            if "*" in args[f]:
+                template = args[f].replace("*", "")
+                found = False
+                for csv in os.listdir(os.getcwd()):
+                    if template in csv:
+                        files.append(csv)
+                        found = True
+                if not found:
+                    print(f"Could not find files based on {template}\nplease make sure the script and the files are in the same directory")
+                    quit()
+            else:
+                print(f"Could not find {args[f]}")
+                quit()
         else:
-            output = f"output{count}.csv"
-            break
-else:
-    output = "output.csv"
+            files.append(args[f])
 
-#writing the data to the output file
-output_data = []
-with open(output, "a") as f:
-    f.write(f"{head[0]},{head[1]}\n")
-    
-    for csv in data:
-        for line in csv:
-            if line != ['']:
-                if line != head:
-                    output_data.append(line)
-    
-    output_data.sort()
-    for line in output_data:
-        f.write(f"{line[0]},{line[1]}\n")
+    data = []
+
+    for f in files: # assembling all the data into one multi-dimensional list
+        with open(f, "r") as csv:
+            content = csv.read().split("\n")
+            for line in range(len(content)):
+                content[line] = content[line].split(",")
+                if not '"' in content[line][0]:
+                    for instance in range(len(content[line])):
+                        if content[line] != ['']:
+                            content[line][instance] = float(content[line][instance])
+                
+            data.append(content)
+
+    head = data[0][0]
+    for f in data:
+        if not f[0] == head:
+            print("The files do not have the same headers")
+            quit()
+
+    #deciding on the name of the output file
+    output = ""
+    if os.path.exists("output.csv"):
+        count = 1
+        while True:
+            if os.path.exists(f"output{count}.csv"):
+                count += 1
+            else:
+                output = f"output{count}.csv"
+                break
+    else:
+        output = "output.csv"
+
+    #writing the data to the output file
+    output_data = []
+    with open(output, "a") as f:
+        f.write(f"{head[0]},{head[1]}\n")
+        
+        for csv in data:
+            for line in csv:
+                if line != ['']:
+                    if line != head:
+                        output_data.append(line)
+        
+        output_data.sort()
+        for line in output_data:
+            f.write(f"{line[0]},{line[1]}\n")
+
+
+if __name__ == "__main__":
+    assemble(sys.argv)
--- graph.py
+++ graph.py
@@ -5,48 +5,52 @@ except ImportError:
     quit()
 import sys
 
-if len(sys.argv) == 1:
-    print("Please enter a file to plot as an argument")
-    quit()
+def graph(args):
+    if len(args) == 1:
+        print("Please enter a file to plot as an argument")
+        quit()
 
 
-# --- defining variables ---
-colors = ["silver", "dodgerblue", "darkorange", "forestgreen"]
-labels = ["floor", "symmetrical", "differential", "asymmetrical"]
-title = "Power Filter 1A"
-xmin, xmax = 1000, 100000000
+    # --- defining variables ---
+    colors = ["silver", "dodgerblue", "darkorange", "forestgreen"]
+    labels = ["floor", "symmetrical", "differential", "asymmetrical"]
+    title = "Power Filter 1A"
+    xmin, xmax = 1000, 100000000
 
 
-head_x = ""
-head_y = ""
-x = []
-y = []
-for csv in range(len(sys.argv)-1):
-    with open(sys.argv[csv+1], "r") as f:
-        x.append([])
-        y.append([])
-        for line in f:
-            if line != ['']:
-                line = line.split(",")
-                if '"' in line[0]:
-                    head_x = line[0].replace('"', '')
-                    head_y = line[1].replace('"', '')
-                else:
-                    x[csv].append(float(line[0]))
-                    y[csv].append(float(line[1]))
+    head_x = ""
+    head_y = ""
+    x = []
+    y = []
+    for csv in range(len(args)-1):
+        with open(args[csv+1], "r") as f:
+            x.append([])
+            y.append([])
+            for line in f:
+                if line != ['']:
+                    line = line.split(",")
+                    if '"' in line[0]:
+                        head_x = line[0].replace('"', '')
+                        head_y = line[1].replace('"', '')
+                    else:
+                        x[csv].append(float(line[0]))
+                        y[csv].append(float(line[1]))
 
-fig = plt.figure(figsize=(8,6))
-plot = fig.add_subplot()
+    fig = plt.figure(figsize=(8,6))
+    plot = fig.add_subplot()
 
-for count in range(len(sys.argv)-1):
-    plot.plot(x[count], y[count], colors[count], label=labels[count])
+    for count in range(len(args)-1):
+        plot.plot(x[count], y[count], colors[count], label=labels[count])
 
-plot.set_xlabel(head_x)
-plot.set_ylabel(head_y)
-plt.title(title)
-plt.xlim([xmin, xmax])
-plt.xscale('log')
-plt.grid(True, which="major", color="grey")
-plt.grid(True, which="minor", color="gainsboro")
-plt.legend(loc="lower right")
-plt.show()
+    plot.set_xlabel(head_x)
+    plot.set_ylabel(head_y)
+    plt.title(title)
+    plt.xlim([xmin, xmax])
+    plt.xscale('log')
+    plt.grid(True, which="major", color="grey")
+    plt.grid(True, which="minor", color="gainsboro")
+    plt.legend(loc="lower right")
+    plt.show()
+
+if __name__ == "__main__":
+    graph(sys.argv)
